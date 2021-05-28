4981bc37112ab5b122d4461d350c12a6f3b1d12e
Triest,Tristan T.R. van <tristantriest@gmail.com>
2021-05-28 12:11:18 +0200

    Added live bus support.

--- client/src/App.js
+++ client/src/App.js
@@ -1,105 +1,24 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { io } from 'socket.io-client';
-import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = "pk.eyJ1IjoiYXJpbGl0aCIsImEiOiJja29xODZiMHcwMjVjMnZvaHI5bGh3bjJ6In0.ZTVaklGwPP-wm9gNyfi6tA"
+import React, { useEffect, useState, useRef } from "react";
+import { io } from "socket.io-client";
+import Map from "./components/map";
 
 export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(4.966336589911254);
-  const [lat, setLat] = useState(51.952161496430836);
-  const [zoom, setZoom] = useState(9);
   const [response, setResponse] = useState();
-
   useEffect(() => {
-
-    console.log("Connecting to websocket...")
-    const socket = io("ws://localhost:3001")
+    console.log("Connecting to websocket...");
+    const socket = io("wss://taiova.trvtserver.nl:3001");
     socket.on("connect", () => {
       socket.on("ovdata", (data) => {
-        console.log(data)
-        setResponse(data)
-      })
-    })
-  }, [])
-
-  useEffect(() => {
-    
-  }, [response])
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-      map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
+        setResponse(data);
+      });
     });
+  }, []);
 
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-
-    map.current.on('load', function () {
-      // Insert the layer beneath any symbol layer.
-      var layers = map.current.getStyle().layers;
-      var labelLayerId;
-      for (var i = 0; i < layers.length; i++) {
-        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-          labelLayerId = layers[i].id;
-          break;
-        }
-      }
-      map.current.addLayer(
-      {
-        'id': 'add-3d-buildings',
-        'source': 'composite',
-        'source-layer': 'building',
-        'filter': ['==', 'extrude', 'true'],
-        'type': 'fill-extrusion',
-        'minzoom': 15,
-        'paint': {
-          'fill-extrusion-color': '#aaa',
-          'fill-extrusion-height': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'height']
-          ],
-          'fill-extrusion-base': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'min_height']
-          ],
-          'fill-extrusion-opacity': 1
-        }
-      }, labelLayerId);
-    });
-  });
+  useEffect(() => {}, [response]);
 
   return (
     <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-      <ul>
-        {response && response.map(data => {
-          return <li key={data.vehicleId}>{data.vehicleData.position}</li>
-        })}
-      </ul>
+      <Map data={response !== undefined && response} />
     </div>
   );
 }
-
--- server/index.js
+++ server/index.js
@@ -1,168 +1,245 @@
 /* --------------------
         IMPORTS
 ----------------------*/
-const express = require('express')
-const zmq = require('zeromq')
-const zlib = require('zlib')
-const mongoose = require('mongoose')
-const xml = require('fast-xml-parser')
-const fs = require('fs')
+const express = require("express");
+const zmq = require("zeromq");
+const zlib = require("zlib");
+const axios = require("axios");
+const mongoose = require("mongoose");
+const xml = require("fast-xml-parser");
+const fs = require("fs");
 const io = require("socket.io")(3001, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 const http = require("http");
-const convert = require('./convertToLatLong')
-
+const convert = require("./convertToLatLong");
+const BusInformation = require("./api/GTFSConvert");
+const trips = require("./api/Trips");
 /* --------------------
         CONSTANTS
 ----------------------*/
-const app = express()
-const port = process.env.port || 5000
-const sock = zmq.socket('sub');
+const app = express();
+const port = process.env.port || 5000;
+const sock = zmq.socket("sub");
 const router = express.Router();
 
 /* --------------------
     DATABASE CONFIG
 ----------------------*/
-let db
-let vehicleSchema
-let vehicleModel
+let db;
+let vehicleSchema;
+let vehicleModel;
 
 /* --------------------
     APP CONFIG
 ----------------------*/
-const refreshRateInMilliseconds = 5000;
+const refreshRateInMilliseconds = 2000;
 
-const addVehicleToDatabase = vehicle => {
+const addVehicleToDatabase = (vehicle) => {
+  findVehicle(vehicle.vehicleNumber).then((result) => {
+    if (result != []) return;
+  });
 
-    const newVehicle = new vehicleModel({
-        vehicleId: vehicle.vehiclenumber,
-        vehicleData: {...vehicle, position: convert.rd2wgs(vehicle["rd-x"], vehicle["rd-y"])}
-    })
+  const newVehicle = new vehicleModel({
+    vehicleData: vehicle,
+    vehicleId: vehicle.vehicleNumber,
+    type: vehicle.status,
+  });
 
-    newVehicle.save(error => {
-        if(error) console.error(error)
-    })
-}
+  newVehicle.save((error) => {
+    if (error) console.error(error);
+  });
+};
 
-const findVehicle = async vehicleId => {
-    return await vehicleModel.find({ vehicleId: vehicleId })
-}
+const findVehicle = async (vehicleId, company) => {
+  return await vehicleModel.find({
+    vehicleId: vehicleId,
+    "vehicleData.company": company,
+  });
+};
 
 const getAllVehicles = async () => {
-    return await vehicleModel.find({})
-}
+  return await vehicleModel.find({ type: { $ne: "END" } });
+};
 
-const writeVehiclePositionToDatabase = (vehicleToUpdate, updatedVehicleData) => {
-    vehicleToUpdate.vehicleData = updatedVehicleData
-    vehicleToUpdate.save().then(savedDoc => {
-        vehicleToUpdate === savedDoc ? null : console.log("Error updating vehicle")
+const updateVehicle = (vehicleToUpdate, updatedVehicleData) => {
+  const oldPos = vehicleToUpdate.vehicleData.position;
+  vehicleToUpdate.vehicleData = updatedVehicleData;
+  vehicleToUpdate.type = updatedVehicleData.status;
+
+  if (vehicleToUpdate.vehicleData.status != "ONROUTE")
+    vehicleToUpdate.vehicleData.position = oldPos;
+
+  vehicleToUpdate.save().then((savedDoc) => {
+    vehicleToUpdate === savedDoc ? null : console.log("Error updating vehicle");
+  });
+};
+
+const convertJSONToDatabaseData = (json) => {
+  const kv6posinfo = json.VV_TM_PUSH.KV6posinfo;
+
+  const busses = [];
+  kv6posinfo != undefined &&
+    Object.entries(kv6posinfo).forEach(([key, value]) => {
+      //If is an array of objects
+      if (value[1] !== undefined) {
+        value.map((busData) => {
+          const timeStamp = new Date(busData.timestamp);
+          const position = busData["rd-x"]
+            ? convert.rd2wgs(busData["rd-x"], busData["rd-y"])
+            : "0, 0";
+
+          busses.push({
+            company: busData.dataownercode,
+            planningNumber: busData.lineplanningnumber,
+            journeyNumber: busData.journeynumber,
+            timestamp: timeStamp.getTime(),
+            vehicleNumber: busData.vehiclenumber,
+            position: position,
+            status: key,
+          });
+        });
+        //If is single object
+      } else {
+        const timeStamp = new Date(value.timestamp);
+        const position = value["rd-x"]
+          ? convert.rd2wgs(value["rd-x"], value["rd-y"])
+          : "0, 0";
+
+        busses.push({
+          company: value.dataownercode,
+          planningNumber: value.lineplanningnumber,
+          journeyNumber: value.journeynumber,
+          timestamp: timeStamp.getTime(),
+          vehicleNumber: value.vehiclenumber,
+          position: position,
+          status: key,
+        });
+      }
     });
 
-}
+  return busses;
+};
+
+const cutOffTmi8 = (json) => {
+  let stringJson = JSON.stringify(json);
+  stringJson = stringJson.replace(/tmi8:/g, "");
+
+  return JSON.parse(stringJson);
+};
 
 const ovInit = () => {
+  console.log("Starting OV socket.");
 
-    console.log("Starting OV socket.")
+  sock.connect("tcp://pubsub.ndovloket.nl:7658");
+  sock.subscribe("/ARR/KV6posinfo");
+  sock.subscribe("/CXX/KV6posinfo");
+  sock.subscribe("/EBS/KV6posinfo");
+  sock.subscribe("/QBUZZ/KV6posinfo");
+  sock.subscribe("/RIG/KV6posinfo");
+  sock.subscribe("/KEOLIS/KV6posinfo");
 
-    sock.connect('tcp://pubsub.ndovloket.nl:7658')
-    sock.subscribe('/ARR/KV6posinfo')
-    
-    sock.on('message', function () {
-        const message = Array.prototype.slice.call(arguments)
-       
-        address = message[0].toString()
-        var contents = Buffer.concat(message.slice(1));
-       
-        zlib.gunzip(contents, (err, buffer) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            const result = buffer.toString();
-            const json = xml.parse(result)            
-            const vehiclesOnRoute = json.VV_TM_PUSH.KV6posinfo.ONROUTE;
+  // sock.subscribe("/GVB/KV6posinfo"); // ALL OBJECTS INSIDE OBJECT ;(
+  // sock.subscribe("/OPENOV/KV6posinfo"); // ALL POSITIONS -1 -1
 
-            Array.isArray(vehiclesOnRoute) && vehiclesOnRoute.map(vehicleData => {
-                const vehicleId = vehicleData.vehiclenumber;
+  sock.on("message", function () {
+    const message = Array.prototype.slice.call(arguments);
+    address = message[0].toString();
+    var contents = Buffer.concat(message.slice(1));
 
-                findVehicle(vehicleId).then(data => {
-                    vehicleData = {
-                        position: convert.rd2wgs(vehicleData["rd-x"], vehicleData["rd-y"]),
-                        ...vehicleData
-                    }
-                    if(data.length == 0) {
-                        console.log(`No vehicle with id ${vehicleId} found in database! Adding...`)
-                        addVehicleToDatabase(vehicleData)
-                    } else {
-                        console.log(`Vehicle with id ${vehicleId} found in database! Updating...`)
-                        writeVehiclePositionToDatabase(data[0], vehicleData)
-                        
-                    }
-                })
-            })
+    zlib.gunzip(contents, (err, buffer) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const result = buffer.toString();
+      //                    //
+      // UNCOMMENT TO DEBUG //
+      // console.log(JSON.stringify(xml.parse(result)) || "Niets");
+      const json = cutOffTmi8(xml.parse(result));
+      const databaseData = convertJSONToDatabaseData(json);
 
-            //emit()
+      databaseData.map((bus) => {
+        const vehicleNumber = bus.vehicleNumber;
+        let company = bus.company;
 
-            //TODO: Fix empty parses.
+        //DE ULTIEME HACK
+        company == "RIG" ? (company = "RET") : company;
 
+        if (vehicleNumber === undefined) return;
+        findVehicle(vehicleNumber, company).then((result) => {
+          if (result.length == 0) {
+            console.log(
+              `No vehicle found with id ${vehicleNumber} and company: ${company}.  Adding...`
+            );
+            addVehicleToDatabase(bus);
+          } else if (result.length == 1) {
+            console.log(
+              `Vehicle found with id ${vehicleNumber} and company: ${company}. Updating...`
+            );
+            updateVehicle(result[0], bus);
+          }
         });
-    
+      });
     });
-}
+  });
+};
 
 const databaseInit = (url, databaseName) => {
+  console.log(
+    `Connecting to database with name: ${databaseName} at url: ${url}`
+  );
 
-    console.log(`Connecting to database with name: ${databaseName} at url: ${url}`)
+  mongoose.connect(`${url}/${databaseName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  db = mongoose.connection;
 
-    mongoose.connect(`${url}/${databaseName}`, { useNewUrlParser: true, useUnifiedTopology: true });
-    db = mongoose.connection;
+  db.on("error", console.error.bind(console, "Connection error:"));
+  db.once("open", () => {
+    console.log("Connection to database established.");
 
-    db.on('error', console.error.bind(console, 'Connection error:'));
-    db.once('open', () => {
-        console.log('Connection to database established.')
-
-        vehicleSchema = new mongoose.Schema({
-            vehicleId: Number,
-            vehicleData: Object
-        })
-
-        vehicleModel = mongoose.model('Vehicle Position', vehicleSchema);
-
-        ovInit();
-    })
-
-    
-
-}
-
-
-databaseInit('mongodb://localhost:27017', 'ov-info');
-
-io.on("connection", (socket) => {
-    console.log("New client connected");
-
-    const interval = setInterval(() => {
-        console.log("Emitting new data.")
-
-        getAllVehicles().then(vehicles => {
-            socket.emit("ovdata", vehicles)
-        })
-
-    }, refreshRateInMilliseconds);
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-      clearInterval(interval)
+    vehicleSchema = new mongoose.Schema({
+      vehicleId: Number,
+      vehicleData: Object,
+      type: String,
     });
 
+    vehicleModel = mongoose.model("VehiclePositions", vehicleSchema);
 
+    ovInit();
+    //BusInformation.busApiInit();
+    // trips.databaseInit(db);
+  });
+};
+
+databaseInit("mongodb://localhost:27017", "taiova");
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  const interval = setInterval(() => {
+    console.log("Emitting new data.");
+
+    getAllVehicles().then((vehicles) => {
+      socket.emit("ovdata", vehicles);
+    });
+  }, refreshRateInMilliseconds);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
+app.get("/", (req, res) => res.send("Hello Worlds!"));
 
-app.get('/', (req, res) => res.send('Hello Worlds!'))
+app.get("/busses/:company/:number/", (req, res) => {
+  res.send("Hello world!" + JSON.stringify(req.params));
+});
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
