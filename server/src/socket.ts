2542425e0b2235d165658d799fad61bc297abec9
Arilith <tristantriest@gmail.com>
2021-06-06 16:35:56 +0200

    Added more icons, added keolis/syntus support. Map now updates in realtime.

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -11,12 +11,12 @@ export class Websocket {
   private activeSocket : Socket;
   private db : Database;
 
-  constructor(server : Server) {
+  constructor(server : Server, db : Database) {
     this.SocketInit(server);
+    this.db = db;
   }
 
   async SocketInit(server : Server) {
-    this.db = await Database.getInstance().Init().then();
     console.log(`Initalizing websocket`)
 
     this.io = require("socket.io")(server, {
@@ -35,20 +35,31 @@ export class Websocket {
     this.activeSocket = socket;
     console.log("New client connected.");
 
-    const interval = setInterval(() => {
-          console.log("Emitting new data.");
-          this.db.GetAllVehicles().then((vehicles) => {
-            socket.emit("ovdata", vehicles);
-          })
-    }, bus_update_rate);
+    // const interval = setInterval(() => {
+    //       //console.log("Emitting new data.");
+    //       this.db.GetAllVehicles().then((vehicles) => {
+    //         socket.emit("ovdata", vehicles);
+    //       })
+    // }, bus_update_rate);
 
     socket.on("disconnect", () => {
       console.log("Client disconnected");
-      clearInterval(interval);
+      //clearInterval(interval);
     })
   }
 
   SendDeletedVehicles(vehicles : Array<VehicleData>) : void {
     this.io.emit("deletedVehicles", vehicles);
   }
+
+  Emit() {
+    //Small delay to make sure the server catches up.
+    setTimeout(() => {
+      this.db.GetAllVehicles().then((vehicles) => {
+        this.io.emit("ovdata", vehicles);
+      })
+    }, 100)
+    //TODO: 
+  }
+
 }
